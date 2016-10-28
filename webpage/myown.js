edb930c89abac044bfc79345249d8e49d27c4a51
本田裕規 <hiroki@hondahiroshiki-no-MacBook-Pro.local>
2016-10-27 22:43:20 -0700

    affect flexible height to top image

---
+++ webpage/myown.js
@@ -0,0 +1,11 @@
+//読み込み時の表示
+window_load();
+
+//ウィンドウサイズ変更時に更新
+window.onresize = window_load;
+
+//サイズの表示
+function window_load() {
+  document.getElementById('topimage').style.height
+    = window.innerHeight - document.getElementByTagName('header');
+}
